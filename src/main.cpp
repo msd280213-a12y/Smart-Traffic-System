4eee13250603d2a2d5ad515a1b8a18f5ddb1500b
Mridul Sen <msd280213@gmail.com>
2026-06-28 19:19:43 +0530

    Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "======================================" << endl;
+    cout << " SMART TRAFFIC MANAGEMENT SYSTEM " << endl;
+    cout << "======================================" << endl;
+
+    cout << endl;
+
+    cout << "Project Initialized Successfully!" << endl;
+
+    return 0;
+}
